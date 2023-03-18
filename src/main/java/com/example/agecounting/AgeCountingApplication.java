dc52c2c33afd35359a5c7a26e2dea40a9ee13413
Diego Matté <diegocmatte@gmail.com>
2023-03-18 15:20:55 -0300

    First commit

---
+++ src/main/java/com/example/agecounting/AgeCountingApplication.java
@@ -0,0 +1,13 @@
+package com.example.agecounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeCountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgeCountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agecounting/AgeCountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agecounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeCountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
